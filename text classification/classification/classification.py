9affa52010fd8978a7421ac5195898c580ba04da
PapamichMarios <papamichalopoulos.m@gmail.com>
2020-01-26 16:40:49 +0200

    Added svd for svm

--- text classification/classification/classification.py
+++ text classification/classification/classification.py
@@ -1,26 +1,44 @@
 import pandas as pd
+import numpy as np
 from sklearn import preprocessing, svm
+from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, cross_validate
 
 
-def BoW_SVM(train_data):
+def SVM(train_data, SVD):
     # bow vectorization
     count_vectorizer = CountVectorizer(stop_words=ENGLISH_STOP_WORDS)
     X = count_vectorizer.fit_transform(train_data['Content'])
 
+    if SVD:
+        svd = TruncatedSVD(n_components=10, random_state=1)
+        X = svd.fit_transform(X)
+
     # setting the labels array
     le = preprocessing.LabelEncoder()
-    le.fit(train_data["Label"])
-    y = le.transform(train_data["Label"])
+    y = le.fit_transform(train_data["Label"])
 
-    # training model
+    # SVM classifier
     clf = svm.SVC(kernel='linear')
-    clf.fit(X, y)
 
     # 5 fold cross validation
+    print("Attempting 5-fold cross validation...")
     scores = cross_val_score(clf, X, y, cv=5)
+
+    scoring = {
+        'acc': 'accuracy',
+        'prec_macro': 'precision_macro',
+       'rec_macro': 'recall_macro',
+        'f1_macro': 'f1_macro'
+
+    }
+    scores = cross_validate(clf, X, y, cv=5, scoring=scoring, return_train_score=False)
+    print('Accuracy:',  np.mean(scores['test_acc']))
+    print('Precision:', np.mean(scores['test_prec_macro']))
+    print('Recall:',    np.mean(scores['test_rec_macro']))
+    print('F-Measure:', np.mean(scores['test_f1_macro']))
 
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
@@ -30,4 +48,5 @@ if __name__ == "__main__":
     train_data = pd.read_csv('../../dataset/datasets/q1/train.csv', sep=',')
     # train_data = train_data[:500]
 
-    BoW_SVM(train_data)
+    SVM(train_data, SVD=False)
+    SVM(train_data, SVD=True)
